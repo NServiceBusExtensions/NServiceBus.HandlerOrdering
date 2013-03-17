5e5265fb36df7ba7419f8779239119762b20a12c
Simon Cropp <simon.cropp@gmail.com>
2013-03-17 15:27:10 +1100

    make compat with NSB3

---
+++ SampleOnNSB3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ServiceProcess;
+using HandlerOrdering.Sample;
+using NServiceBus;
+using NServiceBus.Installation.Environments;
+
+public class Program : ServiceBase
+{
+	static void Main()
+	{
+		var bus = Configure.With()
+		         .CustomConfigurationSource(new CustomConfigurationSource())
+		         .DefaultBuilder()
+		         .MsmqTransport()
+		         .UnicastBus()
+				 .LoadMessageHandlers()
+					.PurgeOnStartup(false)
+			.JsonSerializer()
+		         .CreateBus()
+		         .Start(() => Configure.Instance.ForInstallationOn<Windows>().Install());
+		bus.SendLocal(new MyMessage());
+		Console.ReadLine();
+	}
+
+}
